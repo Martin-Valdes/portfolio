5b69353f4527457bbac92b4670c8ffb53b915010
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-03-05 13:11:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.scss'
+import HomeContainer from './components/HomeContainer/HomeContainer'
+
+function App() {
+
+  return (
+    <>
+      <div className='appContainer'>
+        <HomeContainer/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BackReact/BackReact.jsx
@@ -0,0 +1,18 @@
+import reactLogo from '../../assets/react.svg'
+
+import "./BackReact.scss"
+
+function BackReact() {
+
+  return (
+    <>
+      <div className='symbolContainer'>
+        <div className='logoReact' target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BackReact
