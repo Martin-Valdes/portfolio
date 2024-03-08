bb28160555fc81d00efa09e6ffd6bea46c176b54
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-03-08 14:30:28 -0300

    finalizadno homeContainer y mejorando efectos en works

--- src/components/HomeContainer/HomeContainer.jsx
+++ src/components/HomeContainer/HomeContainer.jsx
@@ -36,7 +36,8 @@ const HomeContainer = () => {
           <IconosSocials/>
         </address>
            <section className='pPresent'>
-              <p className='pDescription'>Bienvenido a mi porfolio. Soy un apasionado desarrollador front-end con un enfoque en la creación de experiencias web cautivadoras y funcionales. Mi objetivo es combinar mi creatividad y habilidades técnicas para construir sitios web que no solo sean visualmente atractivos, sino también intuitivos y accesibles para todos los usuarios.
+            <h2 className='titlepParrapf'>¡Bienvenido a mi portfolio!</h2>
+              <p className='pDescription'> Soy un apasionado desarrollador front-end con un enfoque en la creación de experiencias web cautivadoras y funcionales. Mi objetivo es combinar mi creatividad y habilidades técnicas para construir sitios web que no solo sean visualmente atractivos, sino también intuitivos y accesibles para todos los usuarios.
                 <br  /><br />Estoy ansioso por colaborar en proyectos apasionantes y desafiantes, donde pueda utilizar mis habilidades para hacer una diferencia real. Si estás buscando a alguien que no solo tenga las habilidades técnicas necesarias, sino también la pasión y la dedicación para crear experiencias web excepcionales, ¡no dudes en ponerte en contacto conmigo!
               </p>
           </section>
