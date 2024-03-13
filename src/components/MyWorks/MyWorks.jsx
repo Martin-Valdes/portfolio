01a8b6ea1a0c1729cc39de8aef7ea3633b1a141d
fernando valdes <valdesmartin@gmail.com>
2024-03-13 15:51:41 -0300

    responsive

--- src/components/MyWorks/MyWorks.jsx
+++ src/components/MyWorks/MyWorks.jsx
@@ -14,7 +14,7 @@ const MyWorks = () => {
     <div  className="worksContainer">
         
         <section className='cardsWorks'>
-          
+       
             <section className='sectionCard'>
               <Link to="https://automotora-prueba.com/">
               <div data-aos="fade-right"
@@ -43,9 +43,24 @@ const MyWorks = () => {
                   data-aos-easing="ease-in-sine" className='logo' src="../../img/tailwind.png" alt="" /> 
                 <div className='symbol'><BackReact /></div>
               </div>
+              <div className=" infoCard bg-grey">
+                <div className="description ">
+                  <div className='fontCards'>
+                    <h2 className="text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">Automotora</h2>
+                    <p className="mt-4 text-white-500">
+                      Este sitio cuenta con autenticacion de usuarios y de administrador con perisos especiales para la edicion de galeria de vehiculos.
+                    </p>
+                    <dl className="mt-2 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 sm:gap-y-16 lg:gap-x-1">
+                        <div  className="border-t border-gray-200 pt-4">
+                        </div>
+                    </dl>
+                  </div>
+                </div>
+              </div>
             </section>
             
           <section className='sectionCard'>
+          <Link to="https://ecommercelectronics.netlify.app">
             <div data-aos="fade-right"
               data-aos-offset="100"
               data-aos-easing="ease-in-sine"
@@ -53,6 +68,7 @@ const MyWorks = () => {
               
               <img src="../../img/ecommerce.gif" alt="" />
             </div>
+            </Link>
             <div className='divIcons'>
                <img data-aos="fade-left"
               data-aos-offset="100"
@@ -60,30 +76,31 @@ const MyWorks = () => {
               <img data-aos="fade-up"
                 data-aos-offset="100"
                 data-aos-easing="ease-in-sine" className='logo' src="../../img/html-5.png" alt="" />
+                <img data-aos="fade-down"
+                  data-aos-offset="100"
+                  data-aos-easing="ease-in-sine" className='logo' src="../../img/firebase.png" alt="" />
               <img data-aos="fade-down"
                 data-aos-offset="100"
                 data-aos-easing="ease-in-sine" className='logo' src="../../img/sass.svg" alt="" />
               <div className='symbol' ><BackReact /></div>
             </div>
+            <div className=" infoCard bg-grey">
+                <div className="description ">
+                  <div className='fontCards'>
+                    <h2 className="text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">eCommerce</h2>
+                    <p className="mt-4 text-white-500">
+                    Poyecto eCommerce con carro de compras aplicando firebase para guardar los pedidos y darles un tratamiento.
+                    </p>
+                    <dl className="mt-2 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 sm:gap-y-16 lg:gap-x-1">
+                        <div  className="border-t border-gray-200 pt-4">
+                          
+                        </div>
+                    </dl>
+                  </div>
+                </div>
+              </div>
           </section>  
-          <section className='sectionCard'>
-            <div data-aos="fade-right"
-              data-aos-offset="100"
-              data-aos-easing="ease-in-sine"
-              class=" card bg-black shadow-md rounded-md p-4">
-              <h3 class="text-lg font-bold mb-2">Título de la Tarjeta</h3>
-              <p class="text-gray-700 mb-4">Contenido de la tarjeta...</p>
-            </div>
-          </section>
-          <section className='sectionCard'>
-            <div data-aos="fade-right"
-              data-aos-offset="100"
-              data-aos-easing="ease-in-sine"
-              class=" card bg-white shadow-md rounded-md p-4">
-              <h3 class="text-lg font-bold mb-2">Título de la Tarjeta</h3>
-              <p class="text-gray-700 mb-4">Contenido de la tarjeta...</p>
-            </div>
-          </section>
+          
           
         </section>
         
